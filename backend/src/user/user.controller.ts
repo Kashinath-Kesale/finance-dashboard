507cdcecd7282a112ce369fb14f28f870638f4a4
Kashinath Kesale <arjunkesale1@gmail.com>
2026-04-01 23:01:37 +0530

    feat: implement user module with CRUD operations and validation

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller,Get,Param,Patch, Post,} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService) {}    
+
+    @Post()
+    createUser(@Body() dto: CreateUserDto) {
+        return this.userService.createUser(dto);
+    }
+
+    @Get()
+    getUsers() {
+        return this.userService.getUsers();
+    }
+
+
+    @Patch(':id')
+    updateUser(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+        return this.userService.updateUser(id, dto);
+    }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,10 +1,40 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) {}
+
+    async createUser(dto: CreateUserDto) {
+        return this.prisma.user.create({
+            data: {
+                name: dto.name,
+                email: dto.email,
+                role: dto.role,
+            }
+        })
+    }
+
+
+    async getUsers() {
+        return this.prisma.user.findMany();
+    }
+
+
+    async updateUser(id: string, dto: UpdateUserDto) {
+        const user = await this.prisma.user.findUnique({
+            where : {id}
+        });
+
+        if(!user) throw new NotFoundException('User not found');
+
+        return this.prisma.user.update({
+            where : {id},
+            data :dto,
+        })
+    }
 }
 
