80336ea14d675386e876ce48761ca91579f66431
Kashinath Kesale <arjunkesale1@gmail.com>
2026-04-01 20:34:54 +0530

    initial project setup with nestjs and prisma

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { RecordsModule } from './records/records.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [UserModule, AuthModule, RecordsModule, DashboardModule, PrismaModule],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
